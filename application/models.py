43ebd4670641297a8cb3a94a4ea9c6a7f421b5d6
Serin <serin0837@yahoo.com>
2021-02-03 14:49:41 +0000

    check model working

--- application/models.py
+++ application/models.py
@@ -1,10 +1,14 @@
 from application import db
 
 
-class Register(db.Model):
-    name = db.Column(db.String(30), nullable=False, primary_key=True)
+class Exercise(db.Model):
+    name = db.Column(db.String(30), nullable=False,  primary_key=True)
+    done = db.Column(db.String(100))
 
 
-# class Run(db.Model):
-#     run_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     km = db.Column(db.Float, nullable=False)
+class Goal(db.Model):
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    goal_name = db.Column(db.String, nullable=False)
+    goal_setdate = db.Column(db.String, nullable=False)
+    goal_finishdate = db.Column(db.String, nullable=False)
+    goal_success = db.Column(db.Boolean, nullable=False)
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from application import app, db
-from application.models import Register
+from application.models import Goal, Exercise
+from application.forms import GoalForm
 
 db.create_all()
 
@@ -9,36 +10,42 @@ db.create_all()
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.form:
-        exercise = Register(name=request.form.get("name"))
+        exercise = Exercise(name=request.form.get("name"),
+                            done=request.form.get("done"))
         db.session.add(exercise)
         db.session.commit()
-    registrees = Register.query.all()
-    return render_template("home.html", registrees=registrees)
+    exercises = Exercise.query.all()
+    return render_template("edit_exercise.html", exercises=exercises)
 
 
 @app.route("/update", methods=["POST"])
 def update():
-    person = Register.query.filter_by(name=request.form.get("oldname")).first()
-    person.name = request.form.get("newname")
+    exercise = Exercise.query.filter_by(
+        name=request.form.get("oldname")).first()
+    exercise.name = request.form.get("newname")
     db.session.commit()
     return redirect("/")
 
 
 @app.route("/delete", methods=["POST"])
 def delete():
-    person = Register.query.filter_by(name=request.form.get("name")).first()
-    db.session.delete(person)
+    exercise = Exercise.query.filter_by(name=request.form.get("name")).first()
+    db.session.delete(exercise)
     db.session.commit()
     return redirect("/")
 
 
-# Adding Run
-# @app.route("/addRun", methods=['GET', 'POST'])
-# def addRun():
-#     form = AddRun()
-#     if form.validate_on_submit():
-#         run_to_add = Product(
-#             km=form.km.data)
-#         db.session.add(run_to_add)
-#         db.session.commit()
-#         return redirect(url_for('addRun'))
+# edit goal
+@app.route("/edit_goal", methods=['GET', 'POST'])
+def editgoal():
+    form = GoalForm()
+    if form.validate_on_submit():
+        goal_to_add = Goal(
+            goal_name=form.goal_name.data,
+            goal_setdate=form.goal_setdate.data,
+            goal_finishdate=form.goal_finishdate.data,
+            goal_success=form.goal_success.data
+        )
+        db.session.add(goal_to_add)
+        db.session.commit()
+        return redirect(url_for('edit_goal'))
