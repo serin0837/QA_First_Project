167a0fc72437bd5d32c18cd3ff5fec19123965fb
Serin <serin0837@yahoo.com>
2021-02-03 15:35:34 +0000

    model set up

--- application/__init__.py
+++ application/__init__.py
@@ -6,8 +6,10 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['QA_DATABASE_CONNECTION']
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['QA_DATABASE_CONNECTION']
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 from application import routes
